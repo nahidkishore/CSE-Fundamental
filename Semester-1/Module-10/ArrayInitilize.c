19d8bcc3862ba3ac2c039d9d04fbe9eb5ca5a0da
Nahid <nahid@Nahids-MacBook-Air.local>
2023-04-25 13:52:30 +0600

    array and array string problem solved on codeforce

--- Semester-1/Module-10/ArrayInitilize.c
+++ Semester-1/Module-10/ArrayInitilize.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 int main()
 {
-  char a[6] = {'n', 'a', 'h','i', 'd'}; // no gurantee
-  //char a[6] = "nahid"; // gurantee
+  // char a[6] = {'n', 'a', 'h','i', 'd'}; // no gurantee // not standard
+  char a[6] = "nahid"; // gurantee/ standard  // increase 1 size for null character space
   int sz = sizeof(a) / sizeof(char);
-  printf("%d", sz);
+  printf("%d\n", sz);
   printf("%s", a);
   return 0;
 }
-
-
 
 // #include <stdio.h>
 // int main()
@@ -21,4 +19,3 @@ int main()
 //   }
 //   return 0;
 // }
-
--- Semester-1/Module-10/test_3.c
+++ Semester-1/Module-10/test_3.c
@@ -7,8 +7,8 @@ int main()
   int sum = 0;
   for (int i = 0; i < strlen(a); i++)
   {
-    sum = sum + (a[i] - '0');
+    sum = sum+(a[i] - '0');
   }
   printf("%d", sum);
   return 0;
-} 
+}
