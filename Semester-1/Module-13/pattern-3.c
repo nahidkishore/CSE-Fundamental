17d2f1ff309d724bee9c9cacb009d18fd98ccd0d
nahidkishore <nahidkishore99@gmail.com>
2023-05-01 18:52:06 +0600

    nested loop with pattern matching

---
+++ Semester-1/Module-13/pattern-3.c
@@ -0,0 +1,24 @@
+/*print it
+ 1
+ 1 2
+ 1 2 3
+ 1 2 3 4
+ 1 2 3 4 5
+*/
+
+#include <stdio.h>
+int main()
+{
+    int n, k = 1;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= k; j++)
+        {
+            printf("%d", j);
+        }
+        k++;
+        printf("\n");
+    }
+    return 0;
+}
